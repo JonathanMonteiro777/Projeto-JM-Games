d76d7ea81573247570d66e88762ae9e220d0b4e1
JonathanMonteiro777 <167100917+JonathanMonteiro777@users.noreply.github.com>
2025-05-26 21:37:26 -0300

    feat: padronização com variáveis CSS e melhorias visuais

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 
-//efeito do botão voltar ao Topo
-
+// botão voltar ao Topo
 function topo(){
     window.scrollTo(
-        { top: 0, left: 0, behavior: "smooth" }
+        { top: 0, left: 0, behavior: 'smooth' }
     )
 }
 
